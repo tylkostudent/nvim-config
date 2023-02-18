8104b431dab7f9b80f9bb25bdef87f65c817b390
lukas f <luk88luk@gmail.com>
2023-02-18 23:27:09 +0100

    minor-changes

---
+++ lua/lukas/plugins-setup.lua
@@ -0,0 +1,88 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer()
+-- true if packer was just installed
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- import packer safely
+local status, packer = pcall(require, "packer")
+  if not status then
+    return
+  end
+
+  -- add list of plugins to install
+  return packer.startup(function(use)
+  
+  -- packer can manage itself
+  use("wbthomason/packer.nvim")
+  -- my plugins
+  use("nvim-lua/plenary.nvim")
+
+  use("kyazdani42/nvim-web-devicons")
+
+  --nvim tree
+  use {
+  'nvim-tree/nvim-tree.lua',
+    requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+  --status line
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+    
+  --colors 
+  use("bluz71/vim-nightfly-guicolors")
+  
+  --fuzzy finder 
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make"})
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x"}) 
+  
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v1.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},         -- Required
+    {'hrsh7th/cmp-nvim-lsp'},     -- Required
+    {'hrsh7th/cmp-buffer'},       -- Optional
+    {'hrsh7th/cmp-path'},         -- Optional
+    {'saadparwaiz1/cmp_luasnip'}, -- Optional
+    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},             -- Required
+    {'rafamadriz/friendly-snippets'}, -- Optional
+  }
+}
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+
+end)
