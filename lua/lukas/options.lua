2579ac660bb697352becc6189b896e50bf7a352e
Lukas F <luk88luk@gmail.com>
2023-12-02 18:34:47 +0100

    all done for now

--- lua/lukas/options.lua
+++ lua/lukas/options.lua
@@ -56,3 +56,8 @@ vim.api.nvim_create_autocmd('textyankpost', {
   group = highlight_group,
   pattern = '*',
 })
+vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
+vim.cmd("colorscheme nightfly")
