9ae0600425c2f177a4086ff0a0524b4b9de407e8
tylkostudent <luk88luk@gmail.com>
2023-12-08 20:03:46 +0100

    added sensible verible ruleset

--- lua/lukas/setup/verible_setup.lua
+++ lua/lukas/setup/verible_setup.lua
@@ -1,3 +1,3 @@
 require('lspconfig').verible.setup{
-  cmd = {'verible-verilog-ls', '--rules_config_search'},
-} 
+  cmd = {'verible-verilog-ls', '--rules=-no-tabs,-always-comb,-banned-declared-name-patterns,-no-trailing-spaces,-explicit-parameter-storage-type,-unpacked-dimensions-range-ordering'},
+}
