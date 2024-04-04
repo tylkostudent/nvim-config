1b91ce18f5fc03aba50122934ff542ee49bbe73c
lukas <luk88luk@gmail.com>
2024-04-04 23:11:28 +0200

    added diagnostic severity sort

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ require("lukas.options")
 require("lukas.remaps")
 vim.cmd([[:TSEnable highlight<CR>]])
 vim.diagnostic.config({
+	severity_sort = true,
 	underline = {severity = vim.diagnostic.severity.ERROR},
 	virtual_text = {severity = vim.diagnostic.severity.ERROR},
 })
