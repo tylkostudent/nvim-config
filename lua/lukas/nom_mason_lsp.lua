7d25391f497127455424adab2e9922c8493c0306
LukasF <luk88luk@gmail.com>
2024-12-05 13:14:50 +0100

    test1

--- lua/lukas/nom_mason_lsp.lua
+++ lua/lukas/nom_mason_lsp.lua
@@ -6,8 +6,22 @@ local gdscript_config = {
 if vim.fn.has("win32") == 1 then
     -- Windows specific. Requires nmap installed (`winget install nmap`)
     gdscript_config["cmd"] = { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" }
-elseif vim.fn.has("wsl") then
-    print("wsl")
+elseif vim.fn.has("wls") then
+    local cmd = { "godot-wsl-lsp" }
+    local pipe = "/tmp/godot.pipe"
+
+    vim.lsp.start({
+        name = "Godot",
+        cmd = cmd,
+        filetypes = { "gdscript" },
+        root_dir = vim.fs.dirname(vim.fs.find({ "project.godot", ".git" }, {
+            upward = true,
+            path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+        })[1]),
+        on_attach = function(client, bufnr)
+            vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
+        end
+    })
 end
 
 require("lspconfig").gdscript.setup(gdscript_config)
