7375baaf49c656f77167e688f6d1195bba0a3b9a
LukasF <luk88luk@gmail.com>
2024-12-05 13:07:11 +0100

    test

--- lua/lukas/nom_mason_lsp.lua
+++ lua/lukas/nom_mason_lsp.lua
@@ -6,6 +6,8 @@ local gdscript_config = {
 if vim.fn.has("win32") == 1 then
     -- Windows specific. Requires nmap installed (`winget install nmap`)
     gdscript_config["cmd"] = { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" }
+elseif vim.fn.has("wsl") then
+    print("wsl")
 end
 
 require("lspconfig").gdscript.setup(gdscript_config)
