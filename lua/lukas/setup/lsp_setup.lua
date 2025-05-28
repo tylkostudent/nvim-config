e60cbcd94db9768ee3ca0a119f58372dc181fe83
Luk <not_my_email@me.com>
2025-05-28 16:53:34 +0200

    gowno

--- lua/lukas/setup/lsp_setup.lua
+++ lua/lukas/setup/lsp_setup.lua
@@ -100,15 +100,15 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
+--mason_lspconfig.setup_handlers {
+--  function(server_name)
+--    require('lspconfig')[server_name].setup {
+--      capabilities = capabilities,
+--      on_attach = on_attach,
+--      settings = servers[server_name],
+--      filetypes = (servers[server_name] or {}).filetypes,
+--    }
+end,
 
 require("lukas.nom_mason_lsp")
 
