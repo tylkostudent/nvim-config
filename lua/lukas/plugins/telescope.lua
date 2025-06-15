a732425eb100b00a5867dfefe723c1d18022e91e
LukasF <luk88luk@gmail.com>
2025-06-15 16:14:32 +0200

    aldiujhja

--- lua/lukas/plugins/telescope.lua
+++ lua/lukas/plugins/telescope.lua
@@ -33,7 +33,7 @@ return {
     'nvim-lua/plenary.nvim',
     { 
       'nvim-telescope/telescope-fzf-native.nvim', 
-      build = 'cmake',
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
       config = function()
         require("telescope").load_extension("fzf")
       end,
