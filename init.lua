c9c9ac30a2460f82e26d874a7b0db595e11c5f16
LukasF <luk88luk@gmail.com>
2025-06-22 15:01:33 +0200

    hover-window-bourder-added

--- init.lua
+++ init.lua
@@ -6,3 +6,9 @@ require("lukas.plugin_remaps")
 require("lukas.floaterminal")
 require("lukas.gogot_lsp")
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "single", -- or "double", "rounded", "solid", "shadow"
+  }
+)
