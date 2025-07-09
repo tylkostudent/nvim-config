9ec5e956e52ee901393761f91d7b80109272a87a
LukasF <luk88luk@gmail.com>
2025-07-09 21:29:00 +0200

    elixir-ls-works-windows

--- lua/lukas/plugins/lsp_config.lua
+++ lua/lukas/plugins/lsp_config.lua
@@ -5,7 +5,26 @@ local lsp_servers = {
   "svlangserver",
   "pyright",
   "pylsp",
+  "elixirls"
 }
+
+
+local elixirls_setup = function(capabilities)
+  local is_windows = vim.loop.os_uname().version:match("Windows")
+  local elixirls_dir = vim.fn.stdpath("data") .. "/mason/packages/elixir-ls"
+  local elixirls_cmd
+
+  if is_windows then
+    elixirls_cmd = elixirls_dir .. "/language_server.bat"
+  else
+    elixirls_cmd = elixirls_dir .. "/language_server.sh"
+  end
+
+  require("lspconfig").elixirls.setup({
+    capabilities = capabilities,
+    cmd = { elixirls_cmd },
+  })
+end
 
 return {
   "mason-org/mason-lspconfig.nvim",
@@ -28,9 +47,13 @@ return {
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
         local lspconfig = require("lspconfig")
         for _, server in ipairs(lsp_servers) do
-          lspconfig[server].setup({
-            capabilities = capabilities
-          })
+          if server == "elixirls" then 
+            elixirls_setup(capabilities)
+          else
+            lspconfig[server].setup({
+              capabilities = capabilities
+            })
+          end
         end
       end
     },
