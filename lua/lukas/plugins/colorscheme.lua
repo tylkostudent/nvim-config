5584ff98d8c654db309e228844474047d10bc7a5
LukasF <luk88luk@gmail.com>
2025-07-14 02:04:39 +0200

    aaaaa

--- lua/lukas/plugins/colorscheme.lua
+++ lua/lukas/plugins/colorscheme.lua
@@ -1,15 +1,16 @@
-
 return {
-  "rose-pine/neovim", 
-  name = "rose-pine", 
-  config = function()
-    require("rose-pine").setup({
-      styles = {
-        italic = false,
-        bold = true,
-        transparency = true,
-      }
-    })
-    vim.cmd.colorscheme("rose-pine")
-  end
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        styles = {
+          italic = false,
+          bold = true,
+          transparency = true,
+        }
+      })
+      vim.cmd.colorscheme("rose-pine")
+    end
+  },
 }
