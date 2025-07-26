45056cdb5905edf2a82f3dc309e8c993bca85b41
LukasF <luk88luk@gmail.com>
2025-07-26 18:20:16 +0200

    aaa

--- lua/lukas/lazy.lua
+++ lua/lukas/lazy.lua
@@ -51,4 +51,13 @@ require('lazy').setup({
   "sindrets/diffview.nvim",
   "darrikonn/vim-gofmt",
   "tpope/vim-fugitive",
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
 })
