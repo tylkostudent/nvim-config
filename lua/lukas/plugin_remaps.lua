423b062c7921525911c9fa749a385d57f8ab18d9
LukasF <luk88luk@gmail.com>
2025-07-27 21:22:14 +0200

    aaab

--- lua/lukas/plugin_remaps.lua
+++ lua/lukas/plugin_remaps.lua
@@ -67,7 +67,7 @@ vim.keymap.set("n", "<leader>fq", require"telescope.builtin".quickfix, {desc = "
 
 vim.keymap.set("n", "ma", require("harpoon.mark").add_file, {desc= "[h]arpoon [a]dd file"})
 vim.keymap.set("n", "ms", require("harpoon.ui").toggle_quick_menu, {desc= "[h]arpoon [s]how files"})
-vim.keymap.set("n", "<leader>sm", "<cmd>Telescope harpoon marks<CR>", {desc= "[s]how [m]arked files by harpoon"})
+vim.keymap.set("n", "mf", "<cmd>Telescope harpoon marks<CR>", {desc= "[s]how [m]arked files by harpoon"})
 vim.keymap.set("n", "mn", require("harpoon.ui").nav_next, {desc= "[h]arpoon [n]ext file"})
 vim.keymap.set("n", "mp", require("harpoon.ui").nav_prev, {desc= "[h]arpoon [p]rev file"})
 
