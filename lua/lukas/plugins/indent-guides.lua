3793f32a26d5372a80ad9a6867be959691dbe378
LukasF <luk88luk@gmail.com>
2025-07-29 15:23:38 +0200

    indent-guides

--- lua/lukas/plugins/indent-guides.lua
+++ lua/lukas/plugins/indent-guides.lua
@@ -1,3 +1,23 @@
 return {
-  'thaerkh/vim-indentguides',
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+
+    opts = {},
+    config = function ()
+        require("ibl").setup {
+            enabled = false,
+            indent = {
+            },
+            whitespace = { highlight = { "Whitespace", "NonText" } },
+            scope = {
+                enabled = false,
+            },
+        }
+        vim.api.nvim_create_autocmd("Filetype", {
+            pattern = {"gdscript", "python"},
+            command = "IBLEnable"
+        })
+    end
 }
