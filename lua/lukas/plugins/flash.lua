0942b866339c971a57f086665baff2a1578e689f
LukasF <luk88luk@gmail.com>
2025-08-06 14:06:53 +0200

    aaa

--- lua/lukas/plugins/flash.lua
+++ lua/lukas/plugins/flash.lua
@@ -6,6 +6,7 @@ return {
   keys = {
     { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "<leader>R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
